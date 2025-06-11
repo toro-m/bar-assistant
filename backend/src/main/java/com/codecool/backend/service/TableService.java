709c6dd6fc6cf036322eb60a3962df20278a5219
polobence <polovicsbence@gmail.com>
2025-06-11 10:53:41 +0200

    implement TableDao interface and MemoryTableDao class

---
+++ backend/src/main/java/com/codecool/backend/service/TableService.java
@@ -0,0 +1,7 @@
+package com.codecool.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TableService {
+}
---
+++ backend/src/main/java/com/codecool/backend/service/dao/MemoryTableDao.java
@@ -0,0 +1,48 @@
+package com.codecool.backend.service.dao;
+
+import com.codecool.backend.model.Table;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryTableDao implements TableDao {
+    private final Map<Integer, Table> tables = new ConcurrentHashMap<>();
+
+    @Override
+    public List<Table> getAllTables() {
+        return new ArrayList<>(tables.values());
+    }
+
+    @Override
+    public Table getTable(int tableNumber) {
+        return tables.get(tableNumber);
+    }
+
+    @Override
+    public boolean addTable(Table table) {
+        if (table == null) {
+            return false;
+        }
+        tables.put(table.getTableNum(), table);
+        return true;
+    }
+
+    @Override
+    public boolean reserveTable(int tableNumber) {
+        Table table = tables.get(tableNumber);
+        if (table == null || table.isReserved()) {
+            return false;
+        }
+        table.setReserved(true);
+        return true;
+    }
+
+    @Override
+    public boolean deleteTable(int tableNumber) {
+        if (!tables.containsKey(tableNumber)) {
+            return false;
+        }
+        tables.remove(tableNumber);
+        return true;
+    }
+}
