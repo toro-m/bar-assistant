7d12cd0370bfc31f777014d4a8797dd097b25094
toro <toro.mark99@gmail.com>
2025-06-23 14:48:11 +0200

    implement bar table service with repository

--- backend/src/main/java/com/codecool/backend/repository/BarTableRepository.java
+++ backend/src/main/java/com/codecool/backend/repository/BarTableRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BarTableRepository extends JpaRepository<BarTable, Long> {
 
+    BarTable findByTableNumber(int tableNumber);
 }
---
+++ backend/src/main/java/com/codecool/backend/service/BarTableService.java
@@ -0,0 +1,36 @@
+package com.codecool.backend.service;
+
+import com.codecool.backend.model.BarTable;
+import com.codecool.backend.repository.BarTableRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BarTableService {
+    private final BarTableRepository barTableRepository;
+
+    public BarTableService(BarTableRepository barTableRepository) {
+        this.barTableRepository = barTableRepository;
+    }
+
+    public List<BarTable> getAllTables() {
+        return barTableRepository.findAll();
+    }
+
+    public Optional<BarTable> getTable(int tableNumber) {
+        BarTable barTable = barTableRepository.findByTableNumber(tableNumber);
+        return Optional.ofNullable(barTable);
+    }
+
+    public void addTable(int availableSeats) {
+        BarTable barTable = new BarTable(availableSeats);
+        barTableRepository.save(barTable);
+    }
+
+    public void deleteTable(int tableNumber) {
+        barTableRepository.delete(barTableRepository.findByTableNumber(tableNumber));
+    }
+
+}
