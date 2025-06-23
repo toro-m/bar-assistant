4af8485b2c5cda8887167787fe69d1d76317656b
toro <toro.mark99@gmail.com>
2025-06-23 14:41:21 +0200

    bar table repository interface created

---
+++ backend/src/main/java/com/codecool/backend/repository/BarTableRepository.java
@@ -0,0 +1,8 @@
+package com.codecool.backend.repository;
+
+import com.codecool.backend.model.BarTable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BarTableRepository extends JpaRepository<BarTable, Long> {
+
+}
