458f23e038d05e5c0cb2006e235d466e5ed47dfa
polobence <polovicsbence@gmail.com>
2025-06-24 10:55:04 +0200

    create reservation repository

---
+++ backend/src/main/java/com/codecool/backend/repository/ReservationRepository.java
@@ -0,0 +1,7 @@
+package com.codecool.backend.repository;
+
+import com.codecool.backend.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+}
