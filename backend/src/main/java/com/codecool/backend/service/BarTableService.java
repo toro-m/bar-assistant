deeb52fe794bbcdab3e06f41e81972e053e2ab57
toro <toro.mark99@gmail.com>
2025-07-22 13:28:21 +0200

    change delete table implementation, table delete funcion delets all reservations to the table

--- backend/src/main/java/com/codecool/backend/service/BarTableService.java
+++ backend/src/main/java/com/codecool/backend/service/BarTableService.java
@@ -4,6 +4,7 @@ import com.codecool.backend.DTO.TableDTO;
 import com.codecool.backend.exception.DuplicateTableNumberException;
 import com.codecool.backend.model.BarTable;
 import com.codecool.backend.repository.BarTableRepository;
+import com.codecool.backend.repository.ReservationRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,11 @@ import java.util.List;
 @Service
 public class BarTableService {
     private final BarTableRepository barTableRepository;
+    private final ReservationRepository reservationRepository;
 
-    public BarTableService(BarTableRepository barTableRepository) {
+    public BarTableService(BarTableRepository barTableRepository, ReservationService reservationService, ReservationRepository reservationRepository) {
         this.barTableRepository = barTableRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     public List<TableDTO> getAllTables() {
@@ -31,17 +34,18 @@ public class BarTableService {
     public TableDTO getTable(int tableNumber) {
         BarTable barTable = barTableRepository.findByTableNumber(tableNumber);
         if (barTable == null) {
-            throw new EntityNotFoundException("Table number " + tableNumber+ " not found");
+            throw new EntityNotFoundException("Table number " + tableNumber + " not found");
         }
         return new TableDTO(barTable.getTableNumber(), barTable.getAvailableSeats());
     }
 
-
+    @Transactional
     public void deleteTable(int tableNumber) {
         BarTable barTable = barTableRepository.findByTableNumber(tableNumber);
         if (barTable == null) {
             throw new EntityNotFoundException("Table cannot be deleted, because table cannot be found.");
         }
+        reservationRepository.deleteReservationsByTableId((barTable.getId()));
         barTableRepository.delete(barTable);
     }
 
@@ -49,26 +53,26 @@ public class BarTableService {
     public BarTable addTable(int tableNumber, int availableSeats) {
         BarTable barTable = new BarTable(tableNumber, availableSeats);
         if (barTableRepository.existsByTableNumber(barTable.getTableNumber())) {
-            throw new DuplicateTableNumberException(tableNumber+" already exists");
+            throw new DuplicateTableNumberException(tableNumber + " already exists");
         }
-         barTableRepository.save(barTable);
+        barTableRepository.save(barTable);
         return barTable;
     }
 
     @Transactional
     public void updateTable(int tableNumber, TableDTO tableUpdate) {
         BarTable existingTable = barTableRepository.findByTableNumber(tableNumber);
-                if(existingTable == null) {
-                    throw new EntityNotFoundException("Table not found");
-                }
+        if (existingTable == null) {
+            throw new EntityNotFoundException("Table not found");
+        }
         if (tableUpdate.tableNumber() != existingTable.getTableNumber() &&
                 barTableRepository.existsByTableNumber(tableUpdate.tableNumber())) {
-            throw new DuplicateTableNumberException(tableUpdate.tableNumber() +" already exists, cannot update");
+            throw new DuplicateTableNumberException(tableUpdate.tableNumber() + " already exists, cannot update");
         }
 
         existingTable.setTableNumber(tableUpdate.tableNumber());
         existingTable.setAvailableSeats(tableUpdate.numOfSeats());
-         barTableRepository.save(existingTable);
+        barTableRepository.save(existingTable);
     }
 
 }
